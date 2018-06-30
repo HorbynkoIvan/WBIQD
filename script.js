6f1f31ffdd4ec03fcafcd9cd72c15db3b1a6b3fe
Ivan <horbynko.i@gmail.com>
2018-06-30 19:46:25 +0300

    connected slick slider lib, style, added arrows for slider from font-awesome

--- script.js
+++ script.js
@@ -13,9 +13,58 @@ $(function () {
 });
 
 
-$(document).ready(function(){
+/*$(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-        items:8,
-        loop:true
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            420:{
+                items:4
+            },
+            1000:{
+                items:8
+            }
+        }
+    });
+});*/
+/*
+chevron-circle-right
+*/
+
+$(document).ready(function () {
+    $('.brands-carousel').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: '<i class="fa fa-angle-right"></i>',
+        prevArrow: '<i class="fa fa-angle-left"></i>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 420,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
