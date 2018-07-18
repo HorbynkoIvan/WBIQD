64085001cbba56fdea460c82ac05efe7b7749893
Ivan <horbynko.i@gmail.com>
2018-07-18 12:03:11 +0300

    start migrate from pure css to sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-gulp.task('sass', function(){
-    gulp.src('./sass/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./css'));
+var gulp = require('gulp'), // Подключаем Gulp
+    sass = require('gulp-sass'); // Подключаем Sass пакет
+
+gulp.task('sass', function() { // Создаем таск "sass"
+    return gulp.src(['sass/**/*.sass', 'sass/**/*.scss']) // Берем источник
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('css')) // Выгружаем результата в папку css
 });
-gulp.task('watch', function(){
-    gulp.watch('./sass/**/*.scss',['sass']);
+
+gulp.task('watch', function() {
+    gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
 });
-gulp.task('default', ['sass','watch']);
+
+gulp.task('default', ['watch']);
